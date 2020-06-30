b4a77b20799c63d46503065961917e6073483baa
Chris Mayer <mayerxc@gmail.com>
2020-06-30 11:44:36 -0400

    Update to scrape tree house brewery

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
 import express, {Application, Request, Response} from 'express';
+import { treeHouseGet, Brewery } from './scrape';
 const app: Application = express();
 const port: number = 3000;
 
 
 app.get('/', (req: Request, res: Response) => {
     res.send('HomePage');
+});
+
+app.get('/beer', (req: Request, resp: Response) => {
+  (async () => {
+    try {
+      const beerList: Brewery[] = await treeHouseGet();
+      console.log(beerList)
+      resp.json(beerList);
+    } catch (error) {
+      resp.json({'status': error});
+    }
+  })();
 });
 
 app.listen(port, err => {
---
+++ src/scrape.ts
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+export interface Brewery {
+  brewery: string;
+  item: string;
+  status: string;
+  price: number;
+}
+
+
+export async function treeHouseGet(): Promise<Brewery[]> {
+  // let beerList: Brewery[];
+  // axios.get('https://www.treehouseonthefly.com/shop')
+  //   .then(
+  //     (resp) => {
+  //       // console.log(resp);
+  //       const $ = cheerio.load(resp.data);
+  //       beerList = $('.grid-meta-wrapper').toArray().map(
+  //         (x) => {
+  //           return {
+  //             brewery: 'Tree House',
+  //             item: $(x).find('.grid-title').text(),
+  //             status: $(x).find('.grid-meta-status').text().replace(/\r?\n|\r/g, '').trim() || 'N/A',
+  //             price: Number($(x).find('.product-price').text().replace(/\r?\n|\r/g, ''))
+  //           }
+  //         }
+  //       );
+  //       console.log('beer list:', beerList);
+
+  //     }
+  //   );
+  // return beerList;
+
+  let beerList: Brewery[];
+  const response = await axios.get('https://www.treehouseonthefly.com/shop');
+  const $ = cheerio.load(response.data);
+  beerList = $('.grid-meta-wrapper').toArray().map(
+    (x) => {
+      return {
+        brewery: 'Tree House',
+        item: $(x).find('.grid-title').text(),
+        status: $(x).find('.grid-meta-status').text().replace(/\r?\n|\r/g, '').trim() || 'N/A',
+        price: Number($(x).find('.product-price').text().replace(/\r?\n|\r/g, ''))
+      }
+    }
+  );
+  console.log('beerList:', beerList);
+  return beerList
+}
+
+// export { treeHouseGet }
